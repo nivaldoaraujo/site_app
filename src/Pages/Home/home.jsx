00cb990bcc26e067ed639165f958394bb68e0e7e
nivaldoaraujo <batista_nba@hotmail.com>
2023-11-29 15:04:45 -0300

    Prineira parte site

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,15 @@
+import Menu from '../../components/Header/menus'
+import Rodape from '../../components/Footer/footer'
+
+
+function home(){
+    return(
+        <>
+        <Menu />
+        <h1>Home</h1>
+        <Rodape />
+        </>
+    )
+}
+
+export default home
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css'
+
+
+function footer(){
+    return(
+        <>
+        <div className="footer">
+            Feito no Curso de FrontEnd - Senai Maracanã - 2023
+        </div>
+        </>
+    )
+}
+
+export default footer
